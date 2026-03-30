42dace025e27a4bf0759b4537d4ed4fae38c3e37
Pablo Dávila <pablitodavila3f@gmail.com>
2026-03-29 21:28:29 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+// App.java
+public class App {
+    public static void main(String[] args) {
+
+        // Creación de dos objetos Mascota
+        Mascota mascota1 = new Mascota("Max", "Perro", 3);
+        Mascota mascota2 = new Mascota("Luna", "Hamster", 2);
+
+        // Mostrar información de cada mascota
+        mascota1.mostrarInfo();
+        mascota2.mostrarInfo();
+    }
+}
---
+++ Mascota.java
@@ -0,0 +1,28 @@
+/*
+Tarea Nro. 13: Creación de una clase "Mascota" con atributos y métodos
+Nombre: Pablo Dávila
+Fecha: 29/03/2026
+*/
+// Mascota.java
+public class Mascota {
+
+    // Atributos
+    String nombre;
+    String especie;
+    int edad;
+
+    // Constructor
+    public Mascota(String nombre, String especie, int edad) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.edad = edad;
+    }
+
+    // Método mostrarInfo()
+    public void mostrarInfo() {
+        System.out.println("Nombre:  " + nombre);
+        System.out.println("Especie: " + especie);
+        System.out.println("Edad:    " + edad + " años");
+        System.out.println("----------------------------");
+    }
+}
